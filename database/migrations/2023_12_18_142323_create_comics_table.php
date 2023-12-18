7441f5284f408ab89a747017cc0b4690a6e64cdc
Alati11 <peppealati@gmail.com>
2023-12-18 15:55:24 +0100

    ADD - migration comic-table

--- database/migrations/2023_12_18_142323_create_comics_table.php
+++ database/migrations/2023_12_18_142323_create_comics_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('image');
+            $table->string('price');
+            $table->string('series');
+            $table->timestamp('sale_date');
+            $table->string('type');
+            $table->text('artists');
+            $table->text('writers');
             $table->timestamps();
         });
     }
