abdc62aeae64d3e7c0de40f4a12974985733148d
Alati11 <peppealati@gmail.com>
2023-12-19 07:21:14 +0100

    ADD - correction index

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/comics', [ComicController::class, 'index'])
-    ->name('comics.index');
+    ->name('comics');
 
